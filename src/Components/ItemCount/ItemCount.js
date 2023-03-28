6a0f5941be69f7c76e744f3134ac030ae070bdab
Juanma <77707900+JuanmaGuariste@users.noreply.github.com>
2023-03-28 15:54:47 -0300

    Modificación de detalle de componentes

--- src/Components/ItemCount/ItemCount.js
+++ src/Components/ItemCount/ItemCount.js
@@ -1,4 +1,5 @@
-
+import {useNavigate } from 'react-router-dom'
+import './ItemCount.scss'
 
 export const ItemCount = ({max, cantidad, setCantidad, handleAgregar}) => {
 
@@ -8,14 +9,21 @@ export const ItemCount = ({max, cantidad, setCantidad, handleAgregar}) => {
     const handleRestar = () => {
         cantidad > 1 && setCantidad(cantidad - 1)
     }
+    const navigate = useNavigate()
+
+    const handleVolver = () => {
+        navigate(-1);
+    }
 
     return (
-        <div className="my-3">   
-            <button onClick={handleRestar} className="btn btn-outline-primary">-</button>
-            <span className="mx-2">{cantidad}</span>
-            <button onClick={handleSumar} className="btn btn-primary">+</button>
-            <br/>
-            <button onClick={handleAgregar} className="btn btn-success">Agregar al carrito</button>
+        <div className="accionesUsuario">
+            <div className="accionesCompra">
+                <button onClick={handleRestar} className="btn btn-outline-primary">-</button>
+                <span className="mx-2">{cantidad}</span>
+                <button onClick={handleSumar} className="btn btn-primary">+</button>                
+            </div>   
+            <button onClick={handleAgregar} className="btn btn-success ">Agregar al carrito</button>    
+            <button onClick={handleVolver} className="btn btn-primary  btn-volver">Volver</button>         
         </div>
     )
 }
--- src/Components/ItemDetail/ItemDetail.js
+++ src/Components/ItemDetail/ItemDetail.js
@@ -6,7 +6,7 @@ import './ItemDetail.scss'
 
 export const ItemDetail = ({ item }) => {
 
-    const {agregarAlCarrito, isInCart} = useContext(CartContext)
+    const { agregarAlCarrito, isInCart } = useContext(CartContext)
 
     const [cantidad, setCantidad] = useState(1)
 
@@ -14,7 +14,7 @@ export const ItemDetail = ({ item }) => {
 
     const handleVolver = () => {
         navigate(-1);
-    }   
+    }
 
     const handleAgregar = () => {
         const newItem = {
@@ -25,20 +25,26 @@ export const ItemDetail = ({ item }) => {
     }
 
     return (
-        <div className="card">
-            <h2>{item.name}</h2> 
-            <img src={item.img} alt={item.name} />
-            <div className="container">                 
+        <div class="tarjeta">
+            <div class="columna-imagen">
+                <img src={item.img} alt={item.name} />
+            </div>
+            <div class="columna-contenido">  
+                <h2>{item.name}</h2>  
                 <p>{item.category}</p>         
                 <p>{item.description}</p>                
-                <p>Precio: ${item.price}</p>
-
-                
-
+                <p>Precio: ${item.price}</p>   
                 {
                     isInCart(item.id)
-                        ?   <Link to="/cart" className="btn btn-success my-2">Terminar mi compra</Link>
-                        // ?   console.log("Está en el carrito")
+                        ?   <div>
+                                <Link to="/cart" className="btn btn-success mx-5 my-4">
+                                    Terminar mi compra
+                                </Link>
+                                <button onClick={handleVolver} className="btn btn-primary  btn-volver">
+                                    Volver
+                                </button>
+                            </div>
+                        
                         :   <ItemCount 
                                 max={item.stock}
                                 cantidad={cantidad}
@@ -46,10 +52,61 @@ export const ItemDetail = ({ item }) => {
                                 handleAgregar={handleAgregar}
                             />
                 }
-
-                <button onClick={handleVolver} className="btn btn-primary">Volver</button>
-            </div>
-        </div>
+                {/* <button onClick={handleVolver} className="btn btn-primary  btn-volver">Volver</button>            */}
+            </div>                
+        </div> 
     )
+
+    //     <div className="card">            
+    //         <img src={item.img} alt={item.name} />
+    //         <div className="container">      
+    //             <h2>{item.name}</h2> 
+    //             <br/>           
+    //             <p>{item.category}</p>         
+    //             <p>{item.description}</p>                
+    //             <p>Precio: ${item.price}</p>               
+
+    //             {
+    //                 isInCart(item.id)
+    //                     ?   <Link to="/cart" className="btn btn-success my-2">Terminar mi compra</Link>
+    //                     // ?   console.log("Está en el carrito")
+    //                     :   <ItemCount 
+    //                             max={item.stock}
+    //                             cantidad={cantidad}
+    //                             setCantidad={setCantidad}
+    //                             handleAgregar={handleAgregar}
+    //                         />
+    //             }
+
+    //             <button onClick={handleVolver} className="btn btn-primary">Volver</button>
+    //         </div>
+    //     </div>
+     
+
+    // return (
+    //     <div className="card">
+    //         <h2>{item.name}</h2> 
+    //         <img src={item.img} alt={item.name} />
+    //         <div className="container">                 
+    //             <p>{item.category}</p>         
+    //             <p>{item.description}</p>                
+    //             <p>Precio: ${item.price}</p>               
+
+    //             {
+    //                 isInCart(item.id)
+    //                     ?   <Link to="/cart" className="btn btn-success my-2">Terminar mi compra</Link>
+    //                     // ?   console.log("Está en el carrito")
+    //                     :   <ItemCount 
+    //                             max={item.stock}
+    //                             cantidad={cantidad}
+    //                             setCantidad={setCantidad}
+    //                             handleAgregar={handleAgregar}
+    //                         />
+    //             }
+
+    //             <button onClick={handleVolver} className="btn btn-primary">Volver</button>
+    //         </div>
+    //     </div>
+    // )
 }
 
